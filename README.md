1757196d1b8e67979ac74a5d5f325d1a6624253d
xiehuc <xiehuc@gmail.com>
2014-03-07 11:50:02 +0800

    update comment
    Signed-off-by: xiehuc <xiehuc@gmail.com>

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ option
 *  `LLVM_RECOMMAND_VERSION` : select which llvm version to build
 *  `OUTPUT_HASPID`          : does llvmprof.out contain a pid for mpi program
 
+argument
+---------
+
+* `-list-all`      : print out all outputs
+* `-value-content` : print out traped value detail content instead of brief report
+
 note
 -----
 
--- include/ValueProfiling.h
+++ include/ValueProfiling.h
@@ -22,17 +22,35 @@
         个捕获的类型选用int32型而不是int64类型,从而降低内存压力.
 
         另外,对于常数,我们也没有必要完整的记录所有信息,我们可以保留它的捕获次数
-        ,而将它对应的内容标记为-1,只占用一个单位,我们就知道这个Value是一个常数,
-        当然,无论何时它的值我们也是能够推导出来的.
+        ,这个的目的是为了减少内存占用,如果不用记录所有的常量的话,减小的记录的量
+        还是非常可观的.
+
+        因此,我们另外使用一组计数器,记录实际写入的数据数量,因此整个的组织是::
+
+            ValueInfo    ---> {...Counters...} //记录捕获的次数
+            ValueContent ---> {...Counters...} //记录实际写入数据的数量
+                              {{... content ...}   //Value1的内容
+                               {... content ...}   //Value2的内容
+                               {... content ...}}  //Value3的内容
+        
+        这样的格式能够有效应对其它内存压缩方式,比如说将相同的数合并到一起,附加
+        记录重复次数等等. 最后压缩后的数据应该能够还原回完整的捕获的内容.
+
+        .. tip::
+            
+            过去使用-1作为间隔,不使用第二组计数器,但是在cgpop中捕获到大量的-1才
+            导致更改为现在的格式.至于为什么cgpop捕获到-1则是以后要去思考原因的
+            话题了.
 
     #.  ValueProfiling的插桩
     
         ValueProfiling的插桩分为两个部分,首先是需要 *选中* 那些感兴趣的Value,使
-        用 ``insertValueTrap`` 函数插入一个函数调用,保证在运行时将value的值复制
-        出来保存到内存中,同时还保持了一个计数器从而统计一共有多少要捕获的Value,
-        另外一个部分则是使用 ``runOnModule`` 在main函数中插入初始化函数,这里正
-        好使用第一步统计出来的数量信息,直接在ir中创建一个大数组(GlobalValue).它
-        的实现并不困难,可以参考LLVM中的源码.
+        用 ``insertValueTrap`` 函数插入一个函数调用
+        ``llvm_profiling_trap_value`` ,该函数的作用是保证在运行时将value的值复
+        制出来保存到内存中,同时还保持了一个计数器从而统计一共有多少要捕获的
+        Value,另外一个部分则是使用 ``runOnModule`` 在main函数中插入初始化函数,
+        这里正好使用第一步统计出来的数量信息,直接在ir中创建一个大数组
+        (GlobalValue).它的实现并不困难,可以参考LLVM中的源码.
         
         需要额外说明的是这里有两种思路,一种是通过LLVM的一点一点的创建ir,把实际
         的代码直接写入bitcode中,另外一种是使用c语言事先在libprofile中写好.实际
@@ -47,9 +65,11 @@
         ir则会非常困难,并且我们的目的是捕获Value,即使拖慢了程序的执行速度也没有
         关系,反正数值还是一样的.
 
-        对于常数,我们不能插入函数调用,同时还要保持计数器的增长,所以只需要调用
-        LLVM内置的 ``IncrementCounterInBlock`` 即可,它会写入ir的add指令增加计数
-        器,同时不会捕获Value的内容.
+        对于常数,我们需要保持计数器的增长,但是不用捕获其内容,所以在
+        ``llvm_profiling_trap_value`` 中添加了一个参数用于标识是否为常数,如果是
+        的话,只增长其计数器而不执行捕获部分的代码.因此计数器的数大于等于实际写
+        入的数据.为了正确的读取数据的确定长度需要使用另外一组计数器和实际写入的
+        数据保持一致.
 
     #.  ValueProfiling的内存组织
 
@@ -72,7 +92,27 @@
 
         对于常数,由于不会捕获内容,所以链表为空.
 
-*/
+    #.  ValueProfiling的读取
+
+        和其它Profiling不同,ValueProfiling不是全局有效的,所以不能像其他
+        Profiling一样通过相同的遍历顺序来建立映射,因此,它是通过试图找寻
+        ``llvm_profiling_trap_value`` 这个函数调用的指令,来建立的映射,因为该指
+        令的第一个参数是index,第二个参数是Value,就可以建立[index] |srarr| value
+        这样的映射了.然后加上llvmprof.out中的数据,其格式为[index] |srarr|
+        {num,contents},因此就可以联立起来消除index,从而建立[value] |srarr|
+        {inst,num,contents}的数据结构,于是读取完成.其中inst是那条call指令的指针
+        ,因为有一些Value是常数,是没有getParent()来找到所在的位置的,只有
+        Instruction才有getParent()来返回所在的BasicBlock.同时inst还包含了index
+        值以便不时之需.
+
+    #.  ValueProfiling的访问时内存结构
+
+        根据上面的内容,使用一个map<Value*,ValueInfo>来记录所有的信息,其中
+        ValueInfo是一个结构体,包含了inst,num,contents. contents的类型是
+        vector<int>记录了捕获的所有的值的内容. 常数的contents为空.判断value是否
+        为常数有多种方法,不一一赘述.
+
+**/
 
 
 namespace llvm{
